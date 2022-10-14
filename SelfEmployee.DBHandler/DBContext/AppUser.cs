c916ac02fe9b10b85c37f3f75cdb1bed65ad8ede
SulmanBinKhurshid <sulman.khurshid@cbtech.com>
2022-10-15 01:44:18 +0500

    Initial Commit

---
+++ SelfEmployee.DBHandler/DBContext/AppUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace SelfEmployee.DBHandler.DBContext;
+
+// Add profile data for application users by adding properties to the AppUser class
+public class AppUser : IdentityUser
+{
+    public AppUser()
+    {
+
+    }
+    public bool IsActive { get; set; }
+    public long? CreatedBy { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public long? UpdatedBy { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+}
+
---
+++ SelfEmployee/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SelfEmployee.DBHandler.DBContext;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("SelfEmployeeContextConnection");
+builder.Services.AddDbContextPool<SelfEmployeeContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<SelfEmployeeContext>();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+    )
+);
+
+
+app.MapRazorPages();
+
+app.Run();
