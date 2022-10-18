931cea8364de020f3d3f8ae580f399dfe1877c99
SulmanBinKhurshid <sulman.khurshid@cbtech.com>
2022-10-19 01:10:44 +0500

    Project Changes

---
+++ SelfEmployee/Controllers/TaskController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SelfEmployee.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult PostTask()
+        {
+            return View();
+        }
+    }
+}
